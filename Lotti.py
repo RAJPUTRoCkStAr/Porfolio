228b653f0eceb0e48f8f6860ebec484a76bc18ac
RAJPUTRoCkStAr <sumitsingh9441@gmail.com>
2024-09-29 17:12:55 +0530

    portfolio

--- Lotti.py
+++ Lotti.py
@@ -1,9 +1,21 @@
 import json
 import requests
 from streamlit_lottie import st_lottie
-def load_lottiefile(filepath:str):
-    with open (filepath, 'r') as f:
-        return json.load(f)
+import requests
+
+def load_lottiefile(filepath: str):
+    if filepath.startswith("http"):
+        # Fetching the JSON file from a URL
+        response = requests.get(filepath)
+        if response.status_code == 200:
+            return response.json()  # Parse the JSON if the request was successful
+        else:
+            raise FileNotFoundError(f"Could not retrieve file from URL: {filepath}")
+    else:
+        # Local file handling
+        with open(filepath, 'r') as f:
+            return json.load(f)
+
 def load_lottieurl(url:str):
     r = requests.get(url)
     if r.status_code != 200:
